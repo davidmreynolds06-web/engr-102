3a7f741fc5f9e2ae4bf7d2f6d732355b381daef5
David Reynolds <davidmreynolds06@gmail.com>
2025-09-02 20:12:30 -0500

    Done?

--- Lab1/follow_directions.py
+++ Lab1/follow_directions.py
@@ -7,7 +7,7 @@
 # Assignment: Lab1 Topic 1
 # Date: 02 09 2025
 import math
-print("This shows the evaluation of (1-cosx)/x^2 evaluated close to x=0");
+print("This shows the evaluation of (1-cos(x))/x^2 evaluated close to x=0");
 print("My guess is undefined");
 print((1 - math.cos(1.0)) / 1.0 ** 2);
 print((1 - math.cos(0.1)) / 0.1 ** 2);
@@ -18,4 +18,4 @@ print((1 - math.cos(0.00001)) / 0.00001 ** 2);
 print((1 - math.cos(0.000001)) / 0.000001 ** 2);
 print((1 - math.cos(0.0000001)) / 0.0000001 ** 2);
 print();
-print("My guess was off");
+print("My guess was off"); # By a strange margin 
--- Lab1/print_math.py
+++ Lab1/print_math.py
@@ -7,7 +7,7 @@
 # Assignment: Lab1 Topic 1
 # Date: 02 09 2025
 import math
-print("Reynolds number is " (9 * 0.0015) / 0.875);
-print("Wavelength is ", 2 * 0.029 * math.sin(0.6108652), " nm");
-print("Production rate is ", 100 / ((1 + 0.8 * 2 * 10) ** (1 / 0.8)), " barrels/day");
-print("Change in velocity is ", 2029 * math.log(11000 / 8300), " m/s");
+print("Reynolds number is ", (9 * 0.0015) / 0.875); #This prints the Reynolds number
+print("Wavelength is ", 2 * 0.029 * math.sin(0.6108652), " nm"); #This prints the wavelength
+print("Production rate is ", 100 / ((1 + 0.8 * 2 * 10) ** (1 / 0.8)), " barrels/day"); #This prints the barrel per day decay rate
+print("Change in velocity is ", 2029 * math.log(11000 / 8300), " m/s"); #This prints the change in velocity
