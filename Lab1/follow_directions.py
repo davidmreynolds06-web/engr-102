af7966d99ccc32dc0316729af2b4f7f208e55440
David Reynolds <davidmreynolds06@gmail.com>
2025-09-02 20:02:36 -0500

    Done?

---
+++ Lab1/follow_directions.py
@@ -0,0 +1,21 @@
+# By submitting this assignment, I agree to the following:
+# "Aggies do not lie, cheat, or steal, or tolerate those who do"
+# "I have not given or received any unauthorized aid on this assignment."
+#
+# Name: David Reynolds
+# Section: ENGR 102
+# Assignment: Lab1 Topic 1
+# Date: 02 09 2025
+import math
+print("This shows the evaluation of (1-cosx)/x^2 evaluated close to x=0");
+print("My guess is undefined");
+print((1 - math.cos(1.0)) / 1.0 ** 2);
+print((1 - math.cos(0.1)) / 0.1 ** 2);
+print((1 - math.cos(0.01)) / 0.01 ** 2);
+print((1 - math.cos(0.001)) / 0.001 ** 2);
+print((1 - math.cos(0.0001)) / 0.0001 ** 2);
+print((1 - math.cos(0.00001)) / 0.00001 ** 2);
+print((1 - math.cos(0.000001)) / 0.000001 ** 2);
+print((1 - math.cos(0.0000001)) / 0.0000001 ** 2);
+print();
+print("My guess was off");
--- Lab1/print_math.py
+++ Lab1/print_math.py
@@ -6,5 +6,8 @@
 # Section: ENGR 102
 # Assignment: Lab1 Topic 1
 # Date: 02 09 2025
-print("Howdy, World!");
-print("This past summer I worked at Philmont Scout ranch in Cimmaron New Mexico.");
+import math
+print("Reynolds number is " (9 * 0.0015) / 0.875);
+print("Wavelength is ", 2 * 0.029 * math.sin(0.6108652), " nm");
+print("Production rate is ", 100 / ((1 + 0.8 * 2 * 10) ** (1 / 0.8)), " barrels/day");
+print("Change in velocity is ", 2029 * math.log(11000 / 8300), " m/s");
