f1be5b68d50dda9096036bfb65b961f045dbbe49
David Reynolds <davidmreynolds06@gmail.com>
2025-09-03 10:17:35 -0500

    Done

--- Lab1/follow_directions.py
+++ Lab1/follow_directions.py
@@ -1,5 +1,5 @@
 # By submitting this assignment, I agree to the following:
-# "Aggies do not lie, cheat, or steal, or tolerate those who do"
+# "Aggies do not lie, cheat, or steal, or tolerate those who do."
 # "I have not given or received any unauthorized aid on this assignment."
 #
 # Name: David Reynolds
--- Lab1/print_math.py
+++ Lab1/print_math.py
@@ -1,5 +1,5 @@
 # By submitting this assignment, I agree to the following:
-# "Aggies do not lie, cheat, or steal, or tolerate those who do"
+# "Aggies do not lie, cheat, or steal, or tolerate those who do."
 # "I have not given or received any unauthorized aid on this assignment."
 #
 # Name: David Reynolds
@@ -7,7 +7,7 @@
 # Assignment: Lab1 Topic 1
 # Date: 02 09 2025
 import math
-print("Reynolds number is ", (9.0 * 0.875) / 0.0015); #This prints the Reynolds number
-print("Wavelength is ", 2 * 0.029 * math.sin(0.6108652), " nm"); #This prints the wavelength
-print("Production rate is ", 100 / ((1 + 0.8 * 2 * 10) ** (1 / 0.8)), " barrels/day"); #This prints the barrel per day decay rate
-print("Change in velocity is ", 2029 * math.log(11000 / 8300), " m/s"); #This prints the change in velocity
+print("Reynolds number is", (9.0 * 0.875) / 0.0015); #This prints the Reynolds number
+print("Wavelength is", 2 * 0.029 * math.sin(0.6108652), "nm"); #This prints the wavelength
+print("Production rate is", 100 / ((1 + 0.8 * 2 * 10) ** (1 / 0.8)), "barrels/day"); #This prints the barrel per day decay rate
+print("Change in velocity is", 2029 * math.log(11000 / 8300), "m/s"); #This prints the change in velocity
