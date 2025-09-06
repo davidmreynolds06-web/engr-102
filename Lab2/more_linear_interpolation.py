0775c506358e1c1c4d1b5a085504a913e3976c3a
David Reynolds <davidmreynolds06@gmail.com>
2025-09-05 22:46:42 -0500

    Work

--- Lab2/more_linear_interpolation.py
+++ Lab2/more_linear_interpolation.py
@@ -7,28 +7,57 @@
 # Assignment: Lab Topic 2 (Independent)
 # Date: 05 09 2025
 import math
-t0 = 12
-x0 = 8
-y0 = 6
-z0 = 7
-t3 = 85
-x3 = -5
-y3 = 30
-z3 = 9
+T0 = 12
+X0 = 8
+Y0 = 6
+Z0 = 7
+T3 = 85
+X3 = -5
+Y3 = 30
+Z3 = 9
 t1 = 30
-x1 = ((x3-x0)/(t3-t0))*(t1-t0)+x0
-y1 = ((y3-y0)/(t3-t0))*(t1-t0)+y0
-z1 = ((z3-z0)/(t3-t0))*(t1-t0)+z0
+x1 = ((X3-X0)/(T3-T0))*(t1-T0)+X0
+y1 = ((Y3-Y0)/(T3-T0))*(t1-T0)+Y0
+z1 = ((Z3-Z0)/(T3-T0))*(t1-T0)+Z0
 print("At time 30.0 seconds:")
 print("x1 =", x1, "m")
 print("y1 =", y1, "m")
 print("z1 =", z1, "m")
+print("-----------------------")
 t2 = 37.5
-x2 = ((x3-x0)/(t3-t0))*(t2-t0)+x0
-y2 = ((y3-y0)/(t3-t0))*(t2-t0)+y0
-z2 = ((z3-z0)/(t3-t0))*(t2-t0)+z0
+x2 = ((X3-X0)/(T3-T0))*(t2-T0)+X0
+y2 = ((Y3-Y0)/(T3-T0))*(t2-T0)+Y0
+z2 = ((Z3-Z0)/(T3-T0))*(t2-T0)+Z0
 print("At time 37.5 seconds:")
 print("x2 =", x2, "m")
 print("y2 =", y2, "m")
 print("z2 =", z2, "m")
+print("-----------------------")
+t3 = 45.0
+x3 = ((X3-X0)/(T3-T0))*(t3-T0)+X0
+y3 = ((Y3-Y0)/(T3-T0))*(t3-T0)+Y0
+z3 = ((Z3-Z0)/(T3-T0))*(t3-T0)+Z0
+print("At time 45.0 seconds:")
+print("x3 =", x3, "m")
+print("y3 =", y3, "m")
+print("z3 =", z3, "m")
+print("-----------------------")
+t4 = 52.5
+x4 = ((X3-X0)/(T3-T0))*(t4-T0)+X0
+y4 = ((Y3-Y0)/(T3-T0))*(t4-T0)+Y0
+z4 = ((Z3-Z0)/(T3-T0))*(t4-T0)+Z0
+print("At time 52.5 seconds:")
+print("x4 =", x4, "m")
+print("y4 =", y4, "m")
+print("z4 =", z4, "m")
+print("-----------------------")
+t5 = 60.0
+x5 = ((X3-X0)/(T3-T0))*(t5-T0)+X0
+y5 = ((Y3-Y0)/(T3-T0))*(t5-T0)+Y0
+z5 = ((Z3-Z0)/(T3-T0))*(t5-T0)+Z0
+print("At time 60.0 seconds:")
+print("x5 =", x5, "m")
+print("y5 =", y5, "m")
+print("z5 =", z5, "m")
+print("-----------------------")
 #This is a little much
