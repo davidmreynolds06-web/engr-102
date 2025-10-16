a1156489bbc9cc2e1a3c27c9f84cb3e60844b713
David Reynolds <davidmreynolds06@gmail.com>
2025-10-15 22:51:30 -0500

    Fixed

--- Lab7/split_list.py
+++ Lab7/split_list.py
@@ -11,7 +11,7 @@ num_list = [int(x) for x in input("Enter numbers: ").split()]
 middle_index = len(num_list) // 2
 
 first_half = num_list[:middle_index]
-second_half = num_list[middle_index:]
+second_half = num_list[middle_index:] 
 
 left_sum = 0
 for num in first_half:
@@ -25,7 +25,40 @@ if left_sum == right_sum:
     print("Left:", first_half)
     print("Right:", second_half)
     print("Both sum to", left_sum)
-else: 
-    print("Cannot split evenly")
+elif left_sum > right_sum:
+    second_half = [first_half[-1]] + second_half
+    first_half = first_half[:-1]
+    left_sum = 0
+    for num in first_half:
+        left_sum += num
+
+    right_sum = 0
+    for num in second_half:
+        right_sum += num
+
+    if left_sum == right_sum:
+        print("Left:", first_half)
+        print("Right:", second_half)
+        print("Both sum to", left_sum)
+    else: 
+        print("Cannot split evenly")
+elif right_sum > left_sum:
+    first_half += [second_half[0]]
+    second_half = second_half[1:]
+    left_sum = 0
+    for num in first_half:
+        left_sum += num
+
+    right_sum = 0
+    for num in second_half:
+        right_sum += num
+
+    if left_sum == right_sum:
+        print("Left:", first_half)
+        print("Right:", second_half)
+        print("Both sum to", left_sum)
+    else: 
+        print("Cannot split evenly")
+    
 # Split list
 # Lab 7
