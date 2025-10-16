625f1c1d4d469c030ed4c972f833b9b39705d564
David Reynolds <davidmreynolds06@gmail.com>
2025-10-15 20:45:23 -0500

    Edits

---
+++ Lab7/name_game.py
@@ -0,0 +1,18 @@
+x = input("What is your name? ")
+Vow = "AEIOUYaeiouy"
+
+if x[0].upper() in Vow:
+    y =x[0].lower() + x[1:]
+    print("Your name is " + x + ", " + x + ", Bo-B" + y + "\nBanana-Fana Fo-F" + y + "\nMe-Mi-Mo-M" + y + "\n" + x + "!")
+else:
+    if x[1].lower() in Vow:
+        y = x[1:].lower()
+        print("Your name is " + x + ", " + x + ", Bo-B" + y + "\nBanana-Fana Fo-F" + y + "\nMe-Mi-Mo-M" + y + "\n" + x + "!")
+    else:
+        if x[2].lower() in Vow:
+            y = x[2:].lower()
+            print("Your name is " + x + ", " + x + ", Bo-B" + y + "\nBanana-Fana Fo-F" + y + "\nMe-Mi-Mo-M" + y + "\n" + x + "!")
+        else:
+            y = x[3:].lower()
+            print("Your name is " + x + ", " + x + ", Bo-B" + y + "\nBanana-Fana Fo-F" + y + "\nMe-Mi-Mo-M" + y + "\n" + x + "!")# Name Game
+# Lab 7
