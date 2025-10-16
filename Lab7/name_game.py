7d67ead46c62018d55badfa5c5daaa47b21bf29e
David Reynolds <davidmreynolds06@gmail.com>
2025-10-15 20:53:14 -0500

    Edits

--- Lab7/name_game.py
+++ Lab7/name_game.py
@@ -3,17 +3,17 @@ Vow = "AEIOUYaeiouy"
 
 if x[0].upper() in Vow:
     y =x[0].lower() + x[1:]
-    print(x + ", " + x + ", Bo-B" + y + "\nBanana-Fana Fo-F" + y + "\nMe-Mi-Mo-M" + y + "\n" + x + "!")
+    print(x + ", " + x + ", Bo-B" + y + "\nBanana-Fana Fo-F" + y + "\nMe Mi Mo M" + y + "\n" + x + "!")
 else:
     if x[1].lower() in Vow:
         y = x[1:].lower()
-        print(x + ", " + x + ", Bo-B" + y + "\nBanana-Fana Fo-F" + y + "\nMe-Mi-Mo-M" + y + "\n" + x + "!")
+        print(x + ", " + x + ", Bo-B" + y + "\nBanana-Fana Fo-F" + y + "\nMe Mi Mo M" + y + "\n" + x + "!")
     else:
         if x[2].lower() in Vow:
             y = x[2:].lower()
-            print(x + ", " + x + ", Bo-B" + y + "\nBanana-Fana Fo-F" + y + "\nMe-Mi-Mo-M" + y + "\n" + x + "!")
+            print(x + ", " + x + ", Bo-B" + y + "\nBanana-Fana Fo-F" + y + "\nMe Mi Mo M" + y + "\n" + x + "!")
         else:
             y = x[3:].lower()
-            print(x + ", " + x + ", Bo-B" + y + "\nBanana-Fana Fo-F" + y + "\nMe-Mi-Mo-M" + y + "\n" + x + "!")
+            print(x + ", " + x + ", Bo-B" + y + "\nBanana-Fana Fo-F" + y + "\nMe Mi Mo M" + y + "\n" + x + "!")
 # Name Game
 # Lab 7
