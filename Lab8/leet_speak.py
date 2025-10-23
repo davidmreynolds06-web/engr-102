1e0acd9813262c82aa4c589c67634d8e31e4060a
David Reynolds <davidmreynolds06@gmail.com>
2025-10-22 21:54:43 -0500

    Fixed

--- Lab8/leet_speak.py
+++ Lab8/leet_speak.py
@@ -6,9 +6,9 @@
 # Section: ENGR 102
 # Assignment: Lab Topic 8
 # Date: 22 10 2025
-let_num = {'a': '4', 'e': '3', 'o': '0', 's': '5', 't': '7'}
+let_num = {'a': '4', 'e': '3', 'o': '0', 's': '5', 't': '7'} #dictionary mapping letters to numbers
 text = input("Enter some text: ")
 leet_text = ""
-for char in text:
-    leet_text += let_num.get(char, char)
+for char in text: #iterate through each character in the input text
+    leet_text += let_num.get(char, char) #replace the character with its leet equivalent if it exists
 print(f"In leet speak, \"{text}\" is: {leet_text}")
